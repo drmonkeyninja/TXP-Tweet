65b483677d5d6e21a2aa55813f01fdf907762287
Andy Carter <andy1704@gmail.com>
2014-07-07 22:35:26 +0100

    Added license.

--- arc_twitter.php
+++ arc_twitter.php
@@ -2589,6 +2589,7 @@ h2. Table of contents
 # "Tweeting articles":#arc_twitter_article
 # "The Twitter tab":#arc_twitter_admin
 # "Author":#arc_twitter_author
+# "License":#arc_twitter_license
 
 h2(#arc_twitter_requirements). Plugin requirements
 
@@ -2774,6 +2775,18 @@ h2(#arc_twitter_author). Author
 Contributors: Andy Carter, Phil Wareham and Tommy Schmucker.
 
 Thanks to "Michael Manfre":http://manfre.net/ for inspiration for the article tweet part of this plugin based on his %(tag)mem_twitter% plugin.  Additional thanks to the great Textpattern community for helping to test this plugin and for suggesting new features. The OAuth part of the plugin is thanks to "Abraham Williams":http://twitter.com/abraham.
+
+h2(#arc_twitter_license). License
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Andy Carter
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 # --- END PLUGIN HELP ---
